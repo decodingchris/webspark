6bbc15d4efa7b972a325f4eb2ceffd4ee61bd219
Decoding Chris <decodingchris@gmail.com>
2024-05-01 17:24:45 +0100

    add package.json, gitignore, and organise files and folders

--- README.md
+++ README.md
@@ -2,8 +2,12 @@
 
 ## Test Your Web App Ideas Quickly
 
-WebSpark is a _starter template_ that keep things fast and simple by only using:
+WebSpark is a _web app starter template_ that keep things fast and simple by only using:
 
-- HTML
-- JavaScript
-- Node.js
+- Frontend:
+  - HTML
+  - CSS
+  - JavaScript
+
+- Backend:
+  - Node.js
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
+// Importing required modules
 import { createServer } from "node:http";
-import { URL } from "node:url";
-import fs from "node:fs";
+import { readFile } from "node:fs";
+import { URL, fileURLToPath } from "node:url";
 
 const hostname = "127.0.0.1";
 const port = 8080;
 
+// Function to handle different status codes
 const showStatus = (statusCode, res) => {
   switch (statusCode) {
     case 404:
@@ -21,25 +23,41 @@ const showStatus = (statusCode, res) => {
   }
 };
 
+// Function to serve files
 const serveFile = (filepath, contentType, res) => {
-  fs.readFile(filepath, (err, data) => {
+  // Read the file and handle errors
+  readFile(filepath, "utf-8", (err, data) => {
     if (err) {
+      console.log(err);
       return showStatus(500, res);
     }
+    // Send the file content as response
     res.writeHead(200, { "Content-Type": contentType });
     return res.end(data);
   });
 };
 
+// Function to get full path of a file
+const getFullPath = (filepath) => {
+  return fileURLToPath(new URL(filepath, import.meta.url));
+};
+
+// Create the server
 const server = createServer((req, res) => {
   const myURL = new URL(req.url, `http://${req.headers.host}`);
+  // Serve different files based on the URL pathname
   if (myURL.pathname === "/" && req.method === "GET") {
-    serveFile("./index.html", "text/html", res);
+    serveFile(getFullPath("../frontend/index.html"), "text/html", res);
+  } else if (myURL.pathname === "/style.css" && req.method === "GET") {
+    serveFile(getFullPath("../frontend/style.css"), "text/css", res);
+  } else if (myURL.pathname === "/script.js" && req.method === "GET") {
+    serveFile(getFullPath("../frontend/script.js"), "text/javascript", res);
   } else {
     return showStatus(404, res);
   }
 });
 
+// Start the server
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
